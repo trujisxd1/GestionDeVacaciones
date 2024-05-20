fc39d64252ee52fe15521ad9b6a37fd3c1161aaa
Gustavo Martinez <gutamvol262@gmail.com>
2024-05-20 12:44:05 -0600

    Creacion de el servicio de DepartamentoServices

--- GestionV/src/main/java/com/coneval/gestionv/services/DepartamentoServices.java
+++ GestionV/src/main/java/com/coneval/gestionv/services/DepartamentoServices.java
@@ -1,10 +1,21 @@
 package com.coneval.gestionv.services;
 
 
+import com.coneval.gestionv.entity.Departamento;
+import com.coneval.gestionv.repository.DepartamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DepartamentoServices {
 
+
+    @Autowired
+    private DepartamentoRepository departamentoRepository;
+
+    public void save(Departamento departamento){
+
+        this.departamentoRepository.save(departamento);
+    }
 
 }
