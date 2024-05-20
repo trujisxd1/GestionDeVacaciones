3bdcb3f40ff9493d3b58185a8baa3b0f3da0fa78
Gustavo Martinez <gutamvol262@gmail.com>
2024-05-20 12:33:57 -0600

    Creacion de el servicio de usuario

--- GestionV/src/main/java/com/coneval/gestionv/services/UserServices.java
+++ GestionV/src/main/java/com/coneval/gestionv/services/UserServices.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Primary
@@ -23,5 +24,26 @@ public class UserServices {
     public List<User>listar(){
 
         return this.userRepository.findAll();
+    }
+
+    public void saveUser(User user){
+
+        this.userRepository.save(user);
+    }
+
+    public User buscarPorId(Integer id){
+
+
+        Optional<User>optional=this.userRepository.findById(id);
+
+        if (optional.isPresent()){
+            return optional.get();
+        }
+        return null;
+    }
+
+    public void delete(Integer id){
+
+        this.userRepository.deleteById(id);
     }
 }
