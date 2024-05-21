8d4b4afd29e3c5a4075f57c4c432412e2ec5af3f
Gustavo M <gutamvol262@gmail.com>
2024-05-20 22:11:09 -0600

    agregamos el filtro de buscar por correo

--- GestionV/src/main/java/com/coneval/gestionv/repository/UserRepository.java
+++ GestionV/src/main/java/com/coneval/gestionv/repository/UserRepository.java
@@ -4,8 +4,12 @@ import com.coneval.gestionv.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Integer> {
 
 
+
+    Optional<User>findByEmail(String email);
 
 }
