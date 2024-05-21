e4ec90c1e444e89ec1abd878ea931289cff57df7
Gustavo M <gutamvol262@gmail.com>
2024-05-20 22:10:51 -0600

    agregamos nuevos campos

--- GestionV/src/main/java/com/coneval/gestionv/entity/User.java
+++ GestionV/src/main/java/com/coneval/gestionv/entity/User.java
@@ -1,6 +1,7 @@
 package com.coneval.gestionv.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -43,8 +44,16 @@ public class User {
    @NotBlank
    private String rfc;
 
-   private Date fechaDeIngreso;
 
+    @JsonIgnoreProperties({"handler", "hibernateLazyInitializer"})
+     @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "users_roles",joinColumns = @JoinColumn(name = "user_id")
+            ,inverseJoinColumns = @JoinColumn(name = "role_id"),
+            uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id","role_id"})})
+   private List<Role>roles;
+
+   private Date fechaDeIngreso;
+    @JsonIgnoreProperties({"handler", "hibernateLazyInitializer"})
     @ManyToOne
     @JoinColumn(name = "departamento_id")
     private Departamento departamento;
