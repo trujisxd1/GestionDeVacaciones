64eda6293b40ea3aa4670523a70db65b15e6d9d3
Gustavo Martinez <gutamvol262@gmail.com>
2024-05-27 13:24:50 -0600

    repositorio de cordinaciones

---
+++ GestionV/src/main/java/com/coneval/gestionv/repository/CordinacionRepository.java
@@ -0,0 +1,7 @@
+package com.coneval.gestionv.repository;
+
+import com.coneval.gestionv.entity.Cordinaciones;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CordinacionRepository extends JpaRepository<Cordinaciones,Integer> {
+}
