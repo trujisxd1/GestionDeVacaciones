678fe363cc80a4e0c443cdce1411d2341fd93f7e
Gustavo Martinez <gutamvol262@gmail.com>
2024-05-27 13:25:07 -0600

    agregamos en el servicio los metodos CRUD

--- GestionV/src/main/java/com/coneval/gestionv/services/DepartamentoServices.java
+++ GestionV/src/main/java/com/coneval/gestionv/services/DepartamentoServices.java
@@ -6,6 +6,9 @@ import com.coneval.gestionv.repository.DepartamentoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class DepartamentoServices {
 
@@ -16,6 +19,22 @@ public class DepartamentoServices {
     public void save(Departamento departamento){
 
         this.departamentoRepository.save(departamento);
+    }
+
+    public  List<Departamento>listar(){
+
+        return this.departamentoRepository.findAll();
+    }
+
+    public Departamento buscarPorId(Integer id){
+
+        Optional<Departamento>optional=this.departamentoRepository.findById(id);
+
+        if(optional.isPresent()){
+
+            return optional.get();
+        }
+        return null;
     }
 
 }
