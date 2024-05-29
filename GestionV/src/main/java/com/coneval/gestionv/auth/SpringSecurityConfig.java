c7913faae629d07facd91c00243284eb362c9fca
Gustavo M <gutamvol262@gmail.com>
2024-05-28 21:21:30 -0600

    Configuramos los cors para permitir los metodos CRUD

--- GestionV/src/main/java/com/coneval/gestionv/auth/SpringSecurityConfig.java
+++ GestionV/src/main/java/com/coneval/gestionv/auth/SpringSecurityConfig.java
@@ -4,8 +4,10 @@ package com.coneval.gestionv.auth;
 import com.coneval.gestionv.auth.filter.JwtAuthenticationFilter;
 import com.coneval.gestionv.auth.filter.JwtValidationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -14,6 +16,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 @Configuration
 public class SpringSecurityConfig {
@@ -36,12 +45,40 @@ public class SpringSecurityConfig {
         return http.authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.GET,"/api/v1/listar").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.POST,"/api/v1").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.PUT,"api/v1/editar/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST,"api/v1/vacaciones/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
-                .addFilter(new JwtAuthenticationFilter(authenticationManager()))
-                .addFilter(new JwtValidationFilter(authenticationManager()))
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                         .addFilter(new JwtAuthenticationFilter(authenticationManager()))
+                          .addFilter(new JwtValidationFilter(authenticationManager()))
 
                         .csrf(config -> config.disable())
                          .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                          .build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration=new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("POST","GET","PUT","DELETE","OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization","Cache-Control","Content-Type"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source=new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**",configuration);
+
+        return source;
+
+    }
+
+    @Bean
+    FilterRegistrationBean<CorsFilter>corsfilter(){
+        FilterRegistrationBean<CorsFilter>corsFilter=new FilterRegistrationBean<CorsFilter>(new CorsFilter(this.corsConfigurationSource()));
+
+        corsFilter.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+        return corsFilter;
     }
 }
