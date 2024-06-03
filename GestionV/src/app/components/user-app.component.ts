7b584aa7d544b4e292547aa72e115ba39c43d584
Gustavo Martinez <gutamvol262@gmail.com>
2024-06-03 13:15:47 -0600

    agregamos los campos nuevos  a la tabl

--- GestionV/src/app/components/user-app.component.ts
+++ GestionV/src/app/components/user-app.component.ts
@@ -3,12 +3,14 @@ import { User } from '../models/user';
 import { UserService } from '../services/user.service';
 import { UserComponent } from './user/user.component';
 import { FormUserComponent } from './form-user/form-user.component';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'user-app',
   standalone: true,
   imports: [UserComponent,FormUserComponent],
   templateUrl: './user-app.component.html',
+  providers: [DatePipe]
 
 })
 export class UserAppComponent implements OnInit{
@@ -17,18 +19,28 @@ export class UserAppComponent implements OnInit{
 
   users:User[]=[]
 
-  constructor(private service:UserService){
+  constructor(private service:UserService, private datepipe:DatePipe){
 
 
   }
 
   addUser(user:User){
 
+
+
+    user.fechaDeIngreso=this.formatDate(user.fechaDeIngreso)
   this.users=[... this.users,{...user}]
   }
   ngOnInit(): void {
 
     this.service.findAll().subscribe(users=>this.users=users)
 
+  }
+  formatDate(date: string | null): string {
+    if (date) {
+      const formattedDate = this.datepipe.transform(date, 'dd/MM/yyyy');
+      return formattedDate ? formattedDate : date;
+    }
+    return '';
   }
 }
