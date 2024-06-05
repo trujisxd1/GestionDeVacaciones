7498f7b04752ec730f8a1c20c404a0ccc4e2a0a4
Gustavo M <gutamvol262@gmail.com>
2024-06-04 21:35:57 -0600

    se agregaron las alertas para eliminar usuario para crear y para eliminar

--- GestionV/src/app/components/user-app.component.ts
+++ GestionV/src/app/components/user-app.component.ts
@@ -9,57 +9,70 @@ import Swal from 'sweetalert2';
 @Component({
   selector: 'user-app',
   standalone: true,
-  imports: [UserComponent,FormUserComponent],
+  imports: [UserComponent, FormUserComponent],
   templateUrl: './user-app.component.html',
   providers: [DatePipe]
-
 })
-export class UserAppComponent implements OnInit{
+export class UserAppComponent implements OnInit {
+  title: string = 'Listado de Usuarios';
+  users: User[] = [];
+  userSelected: User;
 
-  title:string ='Listado de Usuarios'
-
-  users:User[]=[]
-  userSelected:User;
-
-  constructor(private service:UserService, private datepipe:DatePipe){
-
-    this.userSelected= new User()
-
+  constructor(private service: UserService, private datepipe: DatePipe) {
+    this.userSelected = new User();
   }
 
-  addUser(user:User){
+  addUser(user: User): void {
+    if (user.id > 0) {
+      this.users = this.users.map(u => u.id === user.id ? { ...user, } : u);
+      Swal.fire({
+        title: "USUARIO ACTUALIZADO",
+        text: "Actualizado con éxito",
+        icon: "success"
+      });
+    } else {
 
-
-
-    user.fechaDeIngreso=this.formatDate(user.fechaDeIngreso)
-  this.users=[... this.users,{...user}]
-
-  Swal.fire({
-    title: "USUARIO CREADO",
-    text: "Creado con exito",
-    icon: "success"
-  });
-  }
-
-  delete(id:number):void{
-
-    this.users=this.users.filter(user => user.id !=id);
-
-  }
-  ngOnInit(): void {
-
-    this.service.findAll().subscribe(users=>this.users=users)
-
-  }
-  formatDate(date: string | null): string {
-    if (date) {
-      const formattedDate = this.datepipe.transform(date, 'dd/MM/yyyy');
-      return formattedDate ? formattedDate : date;
+      this.users = [...this.users, { ...user }];
+      Swal.fire({
+        title: "USUARIO CREADO",
+        text: "Creado con éxito",
+        icon: "success"
+      });
     }
-    return '';
+    this.userSelected = new User();
   }
 
-  setSelectUser(userRow:User):void{
-this.userSelected={... userRow}
+  delete(id: number): void {
+    Swal.fire({
+      title: "Estas seguro de eliminar?",
+      text: "No podras revertir esta accion",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, borrar"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.users = this.users.filter(user => user.id !== id);
+        Swal.fire({
+          title: "Borrado con exito!",
+          text: "Registro borrado",
+          icon: "success"
+        });
+      }
+    });
+
   }
+
+  ngOnInit(): void {
+    this.service.findAll().subscribe(users => this.users = users);
+  }
+
+
+
+  setSelectUser(userRow: User): void {
+    this.userSelected = { ...userRow };
+
+    }
+
 }
