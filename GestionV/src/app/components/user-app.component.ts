43133a54d015a72ff09e70e92444cd999325e128
Gustavo Martinez <gutamvol262@gmail.com>
2024-06-06 12:16:57 -0600

    se agrego el servicio de sharinData para inyectar los metodos y suscribirse a ellos una vez iniciada la app

--- GestionV/src/app/components/user-app.component.ts
+++ GestionV/src/app/components/user-app.component.ts
@@ -1,85 +1,97 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../models/user';
 import { UserService } from '../services/user.service';
-import { UserComponent } from './user/user.component';
-import { FormUserComponent } from './form-user/form-user.component';
 import { DatePipe } from '@angular/common';
 import Swal from 'sweetalert2';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SharingDataService } from '../services/sharing-data.service';
 
 @Component({
   selector: 'user-app',
   standalone: true,
-  imports: [UserComponent, FormUserComponent],
+  imports: [RouterOutlet,NavbarComponent],
   templateUrl: './user-app.component.html',
   providers: [DatePipe]
 })
 export class UserAppComponent implements OnInit {
-  title: string = 'Listado de Usuarios';
+
   users: User[] = [];
   userSelected: User;
-  open:boolean=false
 
-  constructor(private service: UserService, private datepipe: DatePipe) {
+  constructor(private service: UserService, private datepipe: DatePipe,private sharingData:SharingDataService) {
     this.userSelected = new User();
   }
 
-  addUser(user: User): void {
-    if (user.id > 0) {
-      this.users = this.users.map(u => u.id === user.id ? { ...user, } : u);
-      Swal.fire({
-        title: "USUARIO ACTUALIZADO",
-        text: "Actualizado con éxito",
-        icon: "success"
-      });
-    } else {
+  addUser(): void {
 
-      this.users = [...this.users, { ...user }];
-      Swal.fire({
-        title: "USUARIO CREADO",
-        text: "Creado con éxito",
-        icon: "success"
-      });
-    }
-    this.userSelected = new User();
-    this.setOpen()
-  }
+    this.sharingData.newUserEventEmitter.subscribe(user=>{
 
-  delete(id: number): void {
-    Swal.fire({
-      title: "Estas seguro de eliminar?",
-      text: "No podras revertir esta accion",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Si, borrar"
-    }).then((result) => {
-      if (result.isConfirmed) {
-        this.users = this.users.filter(user => user.id !== id);
+      if (user.id > 0) {
+        this.users = this.users.map(u => u.id === user.id ? { ...user, } : u);
         Swal.fire({
-          title: "Borrado con exito!",
-          text: "Registro borrado",
+          title: "USUARIO ACTUALIZADO",
+          text: "Actualizado con éxito",
+          icon: "success"
+        });
+      } else {
+
+        this.users = [...this.users, { ...user }];
+        Swal.fire({
+          title: "USUARIO CREADO",
+          text: "Creado con éxito",
           icon: "success"
         });
       }
-    });
+      this.userSelected = new User();
+    })
+
+  }
+
+  delete(): void {
+
+    this.sharingData.idUserEventEmitter.subscribe(id =>{
+      Swal.fire({
+        title: "Estas seguro de eliminar?",
+        text: "No podras revertir esta accion",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Si, borrar"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this.users = this.users.filter(user => user.id !== id);
+          Swal.fire({
+            title: "Borrado con exito!",
+            text: "Registro borrado",
+            icon: "success"
+          });
+        }
+      });
+
+    })
 
   }
 
   ngOnInit(): void {
     this.service.findAll().subscribe(users => this.users = users);
+    this.addUser()
+    this.delete()
+    this.setSelectUser()
   }
 
 
 
-  setSelectUser(userRow: User): void {
-    this.userSelected = { ...userRow };
-    this.open=true
+  setSelectUser(): void {
+
+    this.sharingData.selectUserEmitter.subscribe(userRow=>{
+
+      this.userSelected = { ...userRow };
+    })
 
     }
 
-    setOpen(){
-      this.open=!this.open
-    }
+
 
 }
