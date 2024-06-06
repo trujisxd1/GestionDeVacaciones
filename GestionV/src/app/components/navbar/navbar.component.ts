94dafecaea45c62cfc3f59c44f1d1fbb98b03060
Gustavo Martinez <gutamvol262@gmail.com>
2024-06-06 12:23:30 -0600

    Nuevo componente con un navbar

---
+++ GestionV/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { User } from '../../models/user';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './navbar.component.html',
+
+})
+export class NavbarComponent {
+
+  @Input() users:User[]=[]
+}
