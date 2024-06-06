328287cd3a739c58bb6832238123b95303313a48
Gustavo Martinez <gutamvol262@gmail.com>
2024-06-06 12:13:19 -0600

    Se agregaron las rutas de las paginas

--- GestionV/src/app/app.routes.ts
+++ GestionV/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { UserComponent } from './components/user/user.component';
+import { FormUserComponent } from './components/form-user/form-user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+
+    path:'',
+    pathMatch:'full',
+    redirectTo:'/usuarios'
+  },
+  {
+      path:'usuarios',
+      component:UserComponent
+    },
+    {
+      path:'usuarios/create',
+      component:FormUserComponent
+    },
+    {
+      path:'usuarios/edit/:id',
+      component:FormUserComponent
+    }
+];
