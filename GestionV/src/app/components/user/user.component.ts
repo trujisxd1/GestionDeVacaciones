219d2ca64c27a97bbe084bbac657cc60f3e23bbf
Gustavo Martinez <gutamvol262@gmail.com>
2024-06-06 12:35:19 -0600

    se inyecto el sharinData y el router

--- GestionV/src/app/components/user/user.component.ts
+++ GestionV/src/app/components/user/user.component.ts
@@ -1,28 +1,41 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { User } from '../../models/user';
+import { Router, RouterModule } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { SharingDataService } from '../../services/sharing-data.service';
 
 
 @Component({
   selector: 'usuarios',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './user.component.html',
 
 })
 export class UserComponent {
+  title: string = 'Listado de Usuarios';
 
-  @Input() users:User[]=[]
+   users:User[]=[]
 
- @Output() idUserEventEmitter = new EventEmitter()
 
- @Output() selectUserEmitter = new EventEmitter()
   onRemove(id: number):void {
 
-    this.idUserEventEmitter.emit(id)
+    this.sharingData.idUserEventEmitter.emit(id)
   }
 
   onSelectedUser(user:User):void{
-    this.selectUserEmitter.emit(user)
+    this.sharingData.selectUserEmitter.emit(user)
+    this.router.navigate(['/usuarios/edit',user.id], {state:{user}})
   }
 
+  constructor(private router:Router, private service:UserService, private sharingData:SharingDataService){
+if(this.router.getCurrentNavigation()?.extras.state){
+
+  this.users=this.router.getCurrentNavigation()?.extras.state!['users']
+
+}else{
+  this.service.findAll().subscribe(users => this.users=users)
+}
+
+  }
 }
