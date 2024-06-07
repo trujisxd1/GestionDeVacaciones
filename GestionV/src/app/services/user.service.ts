b6149f44e87cc431e3e124e164ba44f5af6baf59
Gustavo Martinez <gutamvol262@gmail.com>
2024-06-07 12:23:49 -0600

    se agregaron nuevos consumo de apis

--- GestionV/src/app/services/user.service.ts
+++ GestionV/src/app/services/user.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { User } from '../models/user';
 import { Observable, map, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Puesto } from '../models/puesto';
+import { Cordinacion } from '../models/cordinacion';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +12,31 @@ export class UserService {
 
 
   private users:User[]=[]
+  private puestos:Puesto[]=[]
+  private cordinaciones:Cordinacion[]=[]
   constructor(private http:HttpClient) { }
 
   findAll():Observable<User[]>{
 
-
+    console.log(this.users)
     return this.http.get('http://localhost:8080/api/v1/listar').pipe(
       map((users:any) => users as User[])
+
+
     )
   }
+
+findAllPuesto():Observable<Puesto[]>{
+
+  return this.http.get("http://localhost:8080/api/v1/puestos").pipe(
+    map((puestos:any)=> puestos as Puesto[])
+  )
+}
+
+findAllCordinacion():Observable<Cordinacion[]>{
+
+  return this.http.get("http://localhost:8080/api/v1/cordinaciones/listar").pipe(
+    map((cordinaciones:any)=> cordinaciones as Cordinacion[])
+  )
+}
 }
