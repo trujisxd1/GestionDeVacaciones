3f7857c4b1f663bf64621a52d87061e34d181004
Gustavo M <gutamvol262@gmail.com>
2024-06-06 21:02:42 -0700

    se inyecto el activateRouter para pasar el id en la ruta al igual que se implemento el Oinit para inicializar los datos

--- GestionV/src/app/components/form-user/form-user.component.ts
+++ GestionV/src/app/components/form-user/form-user.component.ts
@@ -1,8 +1,8 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { User } from '../../models/user';
 import { SharingDataService } from '../../services/sharing-data.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -12,14 +12,14 @@ import Swal from 'sweetalert2';
   templateUrl: './form-user.component.html',
 
 })
-export class FormUserComponent {
+export class FormUserComponent implements OnInit {
 
   user:User;
 maxDate: string;
 
 
 
-constructor(private sharingData:SharingDataService, private router:Router){
+constructor(private sharingData:SharingDataService, private route:ActivatedRoute){
 
   this.user= new User()
   const today = new Date();
@@ -27,15 +27,23 @@ constructor(private sharingData:SharingDataService, private router:Router){
   const month = String(today.getMonth() + 1).padStart(2, '0'); // Los meses van de 0 a 11
   const year = today.getFullYear();
   this.maxDate = `${year}-${month}-${day}`
+  this.user = new User()
 
-  if(this.router.getCurrentNavigation()?.extras.state){
 
-    this.user=this.router.getCurrentNavigation()?.extras.state!['user']
-
-  }else{
-    this.user = new User()
-  }
 }
+  ngOnInit(): void {
+
+    this.sharingData.selectUserEvenEmitter.subscribe(user=>this.user=user)
+
+  this.route.paramMap.subscribe(params => {
+
+    const id: number=+(params.get('id')|| '0')
+
+    if(id>0){
+      this.sharingData.findUserByIdEventEmitter.emit(id)
+    }
+  })
+  }
 
 onSubmit(userForm:NgForm):void{
 
