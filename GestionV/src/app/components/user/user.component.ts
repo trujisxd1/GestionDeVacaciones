a665dbee9eb0908d78a200e418a088f338295def
Gustavo Martinez <gutamvol262@gmail.com>
2024-06-13 14:10:14 -0600

    se agregaron iconos y el estilo de tabla resposiva

--- GestionV/src/app/components/user/user.component.ts
+++ GestionV/src/app/components/user/user.component.ts
@@ -1,22 +1,25 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { User } from '../../models/user';
 import { Router, RouterModule } from '@angular/router';
 import { UserService } from '../../services/user.service';
 import { SharingDataService } from '../../services/sharing-data.service';
-
+import { faTrash,faEdit,faUserXmark,faUserPen } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'usuarios',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,FontAwesomeModule],
   templateUrl: './user.component.html',
 
+
 })
-export class UserComponent {
+export class UserComponent implements OnInit{
   title: string = 'Listado de Usuarios';
 
    users:User[]=[]
-
+   trash = faUserXmark;
+   edit=faUserPen
 
   onRemove(id: number):void {
 
@@ -25,17 +28,14 @@ export class UserComponent {
 
   onSelectedUser(user:User):void{
 
-    this.router.navigate(['/usuarios/edit',user.id], {state:{user}})
+    this.router.navigate(['/usuarios/edit',user.id])
   }
 
   constructor(private router:Router, private service:UserService, private sharingData:SharingDataService){
-if(this.router.getCurrentNavigation()?.extras.state){
 
-  this.users=this.router.getCurrentNavigation()?.extras.state!['users']
 
-}else{
-  this.service.findAll().subscribe(users => this.users=users)
-}
-
+  }
+  ngOnInit(): void {
+    this.service.findAll().subscribe(users => this.users=users)
   }
 }
