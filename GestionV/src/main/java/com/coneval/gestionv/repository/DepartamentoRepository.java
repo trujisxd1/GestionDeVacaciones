72c28e09d5a428ab7b97ff5bdaac8231705148ee
Gustavo Martinez <gutamvol262@gmail.com>
2024-06-13 14:02:56 -0600

    se borro el paquete que no se usan

--- GestionV/src/main/java/com/coneval/gestionv/repository/DepartamentoRepository.java
+++ GestionV/src/main/java/com/coneval/gestionv/repository/DepartamentoRepository.java
@@ -2,8 +2,10 @@ package com.coneval.gestionv.repository;
 
 import com.coneval.gestionv.entity.Departamento;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
+
 
 
 public interface DepartamentoRepository extends JpaRepository<Departamento, Integer> {
+
 }
