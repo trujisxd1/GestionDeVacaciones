ff6e38554608d3e6897d8220ada94efc188006ae
Gustavo Martinez <gutamvol262@gmail.com>
2024-06-13 14:11:02 -0600

    se tiparon los emitter

--- GestionV/src/app/services/sharing-data.service.ts
+++ GestionV/src/app/services/sharing-data.service.ts
@@ -1,40 +1,31 @@
 import { EventEmitter, Injectable } from '@angular/core';
-
 import { User } from '../models/user';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharingDataService {
 
- private _newUserEventEmitter:EventEmitter <User> =new EventEmitter()
- private _idUserEventEmitter = new EventEmitter()
-
- private _findUserByIdEventEmitter= new EventEmitter()
-
-private _selectUserEvenEmitter= new EventEmitter()
+  private _newUserEventEmitter: EventEmitter<User> = new EventEmitter();
+  private _idUserEventEmitter = new EventEmitter<number>();
+  private _findUserByIdEventEmitter = new EventEmitter<number>();
+  private _selectUserEvenEmitter: EventEmitter<User> = new EventEmitter();
 
   constructor() { }
 
-  get newUserEventEmitter(){
-    return this._newUserEventEmitter
+  get newUserEventEmitter() {
+    return this._newUserEventEmitter;
   }
 
-  get idUserEventEmitter(){
-
-    return this._idUserEventEmitter
+  get idUserEventEmitter() {
+    return this._idUserEventEmitter;
   }
 
-  get findUserByIdEventEmitter(){
-
-    return this._findUserByIdEventEmitter
+  get findUserByIdEventEmitter() {
+    return this._findUserByIdEventEmitter;
   }
 
-  get selectUserEvenEmitter(){
-
-    return this._selectUserEvenEmitter
+  get selectUserEvenEmitter() {
+    return this._selectUserEvenEmitter;
   }
-
 }
-
