74a549605f8d71c0f611ca50599917a765ec38f9
Gustavo Martinez <gutamvol262@gmail.com>
2024-06-17 13:20:00 -0600

    Se crearon las rutas para vacaciones y se mapearon en el navbar

--- GestionV/src/app/app.routes.ts
+++ GestionV/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { UserComponent } from './components/user/user.component';
 import { FormUserComponent } from './components/form-user/form-user.component';
+import { VacacionesComponent } from './components/vacaciones/vacaciones.component';
 
 export const routes: Routes = [
 
@@ -21,5 +22,9 @@ export const routes: Routes = [
     {
       path:'usuarios/edit/:id',
       component:FormUserComponent
+    },
+    {
+      path:'vacaciones',
+      component:VacacionesComponent
     }
 ];
--- GestionV/src/app/components/navbar/navbar.component.ts
+++ GestionV/src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { User } from '../../models/user';
+import { faPlane, faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,FontAwesomeModule],
   templateUrl: './navbar.component.html',
 
 })
 export class NavbarComponent {
 
+  vac=faPlane
+  plus=faUserPlus
 
 }
