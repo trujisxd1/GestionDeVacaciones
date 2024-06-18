cf402372f0fd587dae62eb6b8f5f6b2ca520a04f
Gustavo Martinez <gutamvol262@gmail.com>
2024-06-18 13:13:22 -0600

    Se agregaron nuevos estilos  a la tablas

--- GestionV/src/app/components/vacaciones/vacaciones.component.ts
+++ GestionV/src/app/components/vacaciones/vacaciones.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Vacaciones } from '../../models/vacaciones';
 import { VacacionesServicesService } from '../../services/vacaciones.services.service';
+import { faFilePdf, faPlaneCircleExclamation, faPlaneCircleXmark, faSuitcaseRolling, faUserPen, faUserPlus, faUserXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 @Component({
   selector: 'app-vacaciones',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './vacaciones.component.html',
   styleUrl: './vacaciones.component.css'
 })
@@ -14,6 +16,10 @@ export class VacacionesComponent implements OnInit{
 
 
 title="Vacaciones"
+trash = faPlaneCircleXmark;
+edit=faPlaneCircleExclamation
+plus=faSuitcaseRolling
+pdf=faFilePdf
 
 vacation:Vacaciones[]=[]
 
