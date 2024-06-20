93cc483c4a4a41a85d6fec5ca62faa6742e01fcb
Gustavo Martinez <gutamvol262@gmail.com>
2024-06-20 13:28:35 -0600

    se agregaron iconos a lapaginacion

--- GestionV/src/app/components/paginador/paginador.component.ts
+++ GestionV/src/app/components/paginador/paginador.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faAngleLeft, faAngleRight, faAnglesLeft, faAnglesRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-paginador',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,FontAwesomeModule],
   templateUrl: './paginador.component.html',
   styleUrl: './paginador.component.css'
 })
@@ -13,5 +15,10 @@ export class PaginadorComponent {
 
   @Input() url:string=''
   @Input() paginador:any={}
+
+  back=faAnglesLeft
+  next=faAnglesRight
+  nextTo=faAngleRight
+  backTo=faAngleLeft
 
 }
