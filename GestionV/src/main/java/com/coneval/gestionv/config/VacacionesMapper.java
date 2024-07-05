2adb85952605905750f3c216aa686538af78a8dd
Gustavo Martinez <gutamvol262@gmail.com>
2024-07-05 13:18:14 -0600

    se hizo un mapper para poder consumir e id  de el usuario asi como su nombre y su apellido en angular

---
+++ GestionV/src/main/java/com/coneval/gestionv/config/VacacionesMapper.java
@@ -0,0 +1,25 @@
+package com.coneval.gestionv.config;
+
+import com.coneval.gestionv.dto.VacacionesDTO;
+import com.coneval.gestionv.entity.Vacaciones;
+import org.springframework.stereotype.Component;
+
+@Component
+public class VacacionesMapper {
+
+    public VacacionesDTO toDTO(Vacaciones vacaciones) {
+        VacacionesDTO dto = new VacacionesDTO();
+        dto.setId(vacaciones.getId());
+        dto.setFechaInicio(vacaciones.getFechaInicio());
+        dto.setFechaFin(vacaciones.getFechaFin());
+        dto.setEstado(vacaciones.getEstado());
+        dto.setPeriodo(vacaciones.getPeriodo());
+        dto.setDiasSolicitados(vacaciones.getDiasSolicitados());
+        dto.setDiasRestantes(vacaciones.getDiasRestantes());
+        dto.setUserId(vacaciones.getUser().getId());
+        dto.setNombreP(vacaciones.getUser().getNombre());
+        dto.setApellidoP(vacaciones.getUser().getApellidoP());
+        dto.setApellidoM(vacaciones.getUser().getApellidoM());
+        return dto;
+    }
+}
