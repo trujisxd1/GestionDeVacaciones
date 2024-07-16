0c30fee72cea354a4f6c6857c64291797ceef367
Gustavo Martinez <gutamvol262@gmail.com>
2024-07-16 11:11:02 -0600

    checamos si el token no a expirado y si puede acceder a las rutas si es que esta autenticado

--- GestionV/src/app/components/vacaciones/vacaciones.component.ts
+++ GestionV/src/app/components/vacaciones/vacaciones.component.ts
@@ -4,7 +4,9 @@ import { VacacionesServicesService } from '../../services/vacaciones.services.se
 import { faFilePdf, faPlaneCircleExclamation, faPlaneCircleXmark, faSuitcaseRolling, faUserPen, faUserPlus, faUserXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { PaginadorComponent } from '../paginador/paginador.component';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import Swal from 'sweetalert2';
+import { SharingDataService } from '../../services/sharing-data.service';
 
 
 @Component({
@@ -26,7 +28,7 @@ pdf=faFilePdf
 
 vacation: Vacaciones[] = [];
 
-constructor(private services: VacacionesServicesService) { }
+constructor(private services: VacacionesServicesService,private router: Router,private sharingData: SharingDataService) { }
 
 ngOnInit(): void {
   this.services.findAll().subscribe(vacaciones => this.vacation = vacaciones);
@@ -43,6 +45,73 @@ downloadPdf(tipo: string, UsuarioId: number, Vacaciones_id: number): void {
     link.download = `vacaciones_${UsuarioId}_${Vacaciones_id}.pdf`;
     link.click();
     window.URL.revokeObjectURL(url);
+  });
+}
+
+editvaca(): void {
+  this.sharingData.newVacationEmitter.subscribe(vaca => {
+    if (vaca && vaca.id > 0) {
+      this.services.update(vaca).subscribe(userUpdate => {
+        if (this.vacation) {
+          this.vacation = this.vacation.map(u => u.id === userUpdate.id ? userUpdate : u);
+        } else {
+          this.vacation = [userUpdate];
+        }
+
+        // Mensaje de depuración
+        console.log("Usuarios después de actualizar:", this.vacation);
+
+        this.router.navigate(['/vacaciones'], {
+
+        }).then(() => {
+          console.log("Redirección después de actualizar el usuario.");
+        });
+
+        Swal.fire({
+          title: "USUARIO ACTUALIZADO",
+          text: "Actualizado con éxito",
+          icon: "success"
+        });
+      });
+    }
+  });
+}
+
+
+onRemove(id:number){
+  Swal.fire({
+    title: "Estas seguro de eliminar?",
+    text: "No podras revertir esta accion",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Si, borrar"
+  }).then((result) => {
+    if (result.isConfirmed) {
+      this.services.remove(id).subscribe(() => {
+        if (this.vacation) {
+          this.vacation = this.vacation.filter(user => user.id != id);
+        }
+
+        // Mensaje de depuración
+        console.log("Usuarios después de eliminar:", this.vacation);
+
+        this.router.navigate(['/usuarios/create'], { skipLocationChange: true }).then(() => {
+          this.router.navigate(['/vacaciones'], {
+
+          }).then(() => {
+            console.log("Redirección después de eliminar el usuario.");
+          });
+        });
+
+        Swal.fire({
+          title: "Borrado con exito!",
+          text: "Registro borrado",
+          icon: "success"
+        });
+      });
+    }
   });
 }
 }
