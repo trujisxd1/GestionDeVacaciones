db19835fcca75a1aba0a754fc8c3097f70b5f026
Gustavo Martinez Trujillo <115821790+trujisxd1@users.noreply.github.com>
2024-07-16 12:40:09 -0600

    Revert "Proyecto de vacaciones Backend echo en java"

--- GestionV/src/main/java/com/coneval/gestionv/repository/CordinacionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.coneval.gestionv.repository;
-
-import com.coneval.gestionv.entity.Cordinaciones;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CordinacionRepository extends JpaRepository<Cordinaciones,Integer> {
-}
