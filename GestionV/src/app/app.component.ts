cdd7a1ed6c647e9f3d701c58a481949c2d465b4d
Gustavo Martinez <gutamvol262@gmail.com>
2024-07-19 11:54:15 -0600

    Tareas por hacer

--- GestionV/src/app/app.component.ts
+++ GestionV/src/app/app.component.ts
@@ -12,5 +12,8 @@ import { UserAppComponent } from './components/user-app.component';
 export class AppComponent {
   title!:string;
 
-
+////////////Tareas Por hacer
+//QUitar los fines de semana
+//agregar cantidad de vacaciones que les sobra
+//cuando se les acabe las vacaciones mandar mensaje de que ya haz ocupado todas 
 }
