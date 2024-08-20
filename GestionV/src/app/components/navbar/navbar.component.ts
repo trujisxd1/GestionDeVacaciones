21f845f98128a239396084e63169fcbb6b81a0c5
Gustavo M <gutamvol262@gmail.com>
2024-08-20 12:01:30 -0600

    Se agregaron los daias restantes

--- GestionV/src/app/components/navbar/navbar.component.ts
+++ GestionV/src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,12 @@
-import { Component, Input } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { User } from '../../models/user';
 import { faCalendarDay, faCalendarPlus, faHouse, faPlane, faR, faRightToBracket, faUserPlus } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AuthService } from '../../services/auth.service';
+import { UserService } from '../../services/user.service';
+import { Vacaciones } from '../../models/vacaciones';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -12,19 +15,41 @@ import { AuthService } from '../../services/auth.service';
   templateUrl: './navbar.component.html',
 
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
 
+  diasre?:Vacaciones
   vac=faPlane
   plus=faUserPlus
  login=faRightToBracket
   create= faCalendarPlus
   calendar=faCalendarDay
+  email: any = this.auth.getUserEmail();
+  vacaciones: Vacaciones[] = [];
 
+  vacacioness: Vacaciones = new Vacaciones();
   home=faHouse
 
-  constructor(private auth:AuthService,private route:Router){
+  constructor(private auth:AuthService,private route:Router,private service: UserService,private activatedRouter:ActivatedRoute){
+    
+  }
+  ngOnInit(): void {
+    this.activatedRouter.params.pipe(
+
+      switchMap(({id})=>this.service.diasRestantes(this.email))
+    ).subscribe(diasre=>{
+
+
+
+        this.diasre=diasre
+
+        console.log({diasre})
+          return
+    })
+
 
   }
+
+
 
   get loginU(){
 return this.auth.user
@@ -41,5 +66,8 @@ return this.auth.user
   @Input() users:User[]=[]
 
   @Input() paginador:any={}
+
+
+
 
 }
