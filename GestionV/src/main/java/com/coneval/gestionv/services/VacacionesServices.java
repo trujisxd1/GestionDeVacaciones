8078c99abd467b0e175bae58e45bf605b1676e54
Gustavo M <gutamvol262@gmail.com>
2024-08-20 11:34:16 -0600

    agregamos la logica para ir disminuyendo las vacaciones

--- GestionV/src/main/java/com/coneval/gestionv/services/VacacionesServices.java
+++ GestionV/src/main/java/com/coneval/gestionv/services/VacacionesServices.java
@@ -9,6 +9,7 @@ import com.coneval.gestionv.repository.VacasionesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -80,19 +81,26 @@ public class VacacionesServices {
     public Optional<Vacaciones> findById(Integer id) {
         return vacasionesRepository.findById(id);
     }
-    public void agregarVacaciones(String emailUsuario, Vacaciones vacaciones) {
-        // Buscar al usuario por su correo electrónico
-        Optional<User> optionalUser = userRepository.findByEmail(emailUsuario);
 
-        if (optionalUser.isPresent()) {
-            User usuario = optionalUser.get();
-            // Asociar las vacaciones con el usuario
-            vacaciones.setUser(usuario);
-            // Guardar las vacaciones en la base de datos
-            vacasionesRepository.save(vacaciones);
+    @Transactional
+    public void agregarVacaciones(String email, Vacaciones vacaciones) {
+        Optional<User> userOptional = userRepository.findByEmail(email);
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            int diasSolicitados = vacaciones.getDiasSolicitados();
+            int diasDisponibles = user.getDiasDisponibles();
+            if (diasDisponibles >= diasSolicitados) {
+                int diasRestantes = diasDisponibles - diasSolicitados;
+                user.setDiasDisponibles(diasRestantes); // Actualizar días disponibles del usuario
+                vacaciones.setUser(user);
+                vacaciones.setDiasRestantes(diasRestantes);
+                vacasionesRepository.save(vacaciones);
+                userRepository.save(user);  // Guardar cambios en el usuario
+            } else {
+                throw new IllegalArgumentException("No tienes suficientes días disponibles.");
+            }
         } else {
-            // Manejar el caso en que el usuario no sea encontrado
-            throw new RuntimeException("Usuario no encontrado para el correo electrónico: " + emailUsuario);
+            throw new IllegalArgumentException("Usuario no encontrado.");
         }
     }
 
